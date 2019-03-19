dbbf6ef08eb2afcd51bc803f410daa67fd8992af
Andrey Volkov <andrew.wolf.alex@gmail.com>
2019-03-19 12:05:18 +0300

    Add tests

--- tracking/tests.py
+++ tracking/tests.py
@@ -1,10 +1,17 @@
+from datetime import timedelta
+
 from django.test import TestCase
-from django.db import IntegrityError
+from django.urls import reverse
 from django.utils import timezone
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 from authentication.models import User
 
-from .models import Tag, Activity, TaggedActivity
+from .models import Tag, Activity, ActivityTagBinding
 
 
 class ActivityModelTestCase(TestCase):
@@ -31,7 +38,12 @@ class ActivityModelTestCase(TestCase):
         )
 
     def test_start_and_end_constraint(self):
-        pass
+        with self.assertRaises(IntegrityError):
+            Activity.objects.create(
+                name='SWP sprint',
+                start=timezone.now(),
+                end=timezone.now() - timedelta(days=1),
+            )
 
     def test_tags_contraint(self):
         activity = Activity.objects.create(
@@ -39,8 +51,46 @@ class ActivityModelTestCase(TestCase):
             start=timezone.now(),
             user=self.first_user,
         )
-        with self.assertRaises(IntegrityError):
-            TaggedActivity.objects.create(
+        with self.assertRaises(ValidationError):
+            ActivityTagBinding.objects.create(
                 activity=activity,
                 tag=self.second_user_tag,
             )
+
+
+class ActivityAPITestCase(APITestCase):
+    def get_response(self, url, user=None, kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+        url = reverse(url, kwargs=kwargs)
+        if user is not None:
+            self.client.force_authenticate(user=user)
+        return self.client.get(url)
+
+    def post_response(self, url, user=None,
+                      kwargs=None, data=None, format='json'):
+        if kwargs is None:
+            kwargs = {}
+        if data is None:
+            data = {}
+        url = reverse(url, kwargs=kwargs)
+        if user is not None:
+            self.client.force_authenticate(user=user)
+        return self.client.post(url, data, format=format)
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='ivan@example.com',
+            first_name="Ivan",
+            second_name="Pavlov",
+            password='qwerty123',
+        )
+
+    def test_empty_activities_list(self):
+        response = self.get_response(
+            'tracking:activity-list',
+            user=self.user,
+        )
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(0, len(response.data))
