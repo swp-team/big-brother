97a15f33626663ac2171f7687ae0acfdb69cc34f
Andrey Volkov <andrew.wolf.alex@gmail.com>
2019-05-03 11:11:04 +0300

    Fix permissions

--- django/tracking/permissions.py
+++ django/tracking/permissions.py
@@ -6,7 +6,6 @@ class StaffPermissions(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-        
         return request.user.is_staff
 
 
@@ -15,12 +14,6 @@ class FacultyPermissions(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         return request.user.is_staff or isinstance(request.user, Faculty)
-
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_staff:
-            return True
-        return self.obj.filter(course__faculties=request.user).exists()
-
 
 
 class StudentPermissions(BasePermission):
